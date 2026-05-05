cc9fb88ef0e8c81f41049c392bac5f39a248688a
SandraHtlr1806 <s2510455002@fhooe.at>
2026-05-05 16:50:31 +0200

    Revert "ci: test CI pipeline"

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module github.com/mrckurz/CI-CD-MCM
 go 1.24
 
 require github.com/gorilla/mux v1.8.1
-
-require github.com/lib/pq v1.12.0
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -102,7 +102,7 @@ func (h *Handler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
 func respondJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	_ = json.NewEncoder(w).Encode(payload)
+	json.NewEncoder(w).Encode(payload)
 }
 
 func respondError(w http.ResponseWriter, code int, message string) {
